7c3939d6564549106f45f59be50ea3354a019156
ssumanss <ssuman44@gmail.com>
2023-01-04 00:13:27 +0530

    starting new website

--- docs/js/app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/python-workshop/sw.js').then(function(registration) {
-    console.log('ServiceWorker registration successful!');
-  }).catch(function(err) {
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
