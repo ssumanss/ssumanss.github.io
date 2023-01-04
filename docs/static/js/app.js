ab39486758ac72a55bb52003a5cebd51eb3209d8
ssumanss <ssuman44@gmail.com>
2023-01-04 22:54:39 +0530

    change made.

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -5,3 +5,20 @@ if ('serviceWorker' in navigator) {
     console.log('ServiceWorker registration failed: ', err);
   });
 }
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
